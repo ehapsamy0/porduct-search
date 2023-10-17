60a4b7d82dd0de7a5b0ddd6ee002b09c09ea3827
ehapsamy0 <ehapsamy434@gmail.com>
2023-10-17 16:02:12 +0300

    dd

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4)j9o7ue-#f$greqbxo=gu#zgsb2xa41@x#czigqe7d=hem7&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
